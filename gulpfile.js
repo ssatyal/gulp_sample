66c3488e94381ad977990ba868e8c10f357a8ab9
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-06-27 16:29:56 -0400

    configures gulp file for jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,18 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    jshint = require('gulp-jshint');
 
-gulp.task('default', function(){
-  return gutil.log('Gulp is running!')
+//defines default task and add the watch task to it
+gulp.task('default', ['watch']);
+
+//configure jshint task
+gulp.task('jshint', function(){
+  return gulp.src('source/javascript/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+//configure which files to watch and what tasks to use on file changes
+gulp.task('watch', function() {
+  gulp.watch('source/javascript/**/*.js', [jshint]);
 })
